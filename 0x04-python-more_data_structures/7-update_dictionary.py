92328ded6e04f93483658597fd1dc9b7972a8e42
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-08-31 10:19:03 +0300

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+ """
+ Replace or add key/value pairs in a dictionary
+ """
+
+
+def update_dictionary(a_dictionary, key, value):
+       a_dictionary[key] = value
+    return (a_dictionary)
