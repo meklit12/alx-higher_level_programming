9c021bb204d774aeb562996ac0628467e452fa5b
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-08-31 13:31:26 +0300

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@
  """
 
 
-def update_dictionary(a_dictionary, key, value):
-       a_dictionary[key] = value
-    return (a_dictionary)
+ef update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
