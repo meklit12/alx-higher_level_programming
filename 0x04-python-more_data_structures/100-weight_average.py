e8140682b4dd5b180e965b0dfe9ee4458926f942
meklit12 <96723774+meklit12@users.noreply.github.com>
2022-08-31 13:23:18 +0300

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+weight average
+"""
+
+
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        num = 0
+        dem = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            dem += tup[1]
+        return (num / dem)
+    return 0
